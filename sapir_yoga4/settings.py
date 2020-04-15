eb780f450ecf7e30bc6223fb0e13211c254149b8
Liad Barnes <LiadBarnes@gmail.com>
2020-04-15 23:11:57 +0300

    fixed picture in thank you page

--- sapir_yoga4/settings.py
+++ sapir_yoga4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=&k@lkyj5su+uv7^0b2^0kz5au5_wbl*a0abko2m7%jnrye%+w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,5 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"))
